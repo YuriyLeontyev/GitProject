653760f5f2e038708c8f25466dac82e0a6627420
YuriyLeontyev <yurale102084@gmail.com>
2022-07-07 23:03:43 +0600

    1_deaf read_project

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+app = FastAPI()
+#uvicorn main:app --reload
+
+# - Сделайте домашний роут и верните словарь (справочник) со следующими данными:
+# site_name, author_name, year. Добавьте информацию о себе.
+# - Сделайте роут /contacts и верните ваши: город, страна, адрес вашей компании.
+# - Сдетайте роут словарь с данными о коллегах, сделайте роут /colleagues/{name}
+# и верните данные о коллеге или тест с ошибкой, если он не найден.
+
+
+@app.get("/project")
+def read_project():
+    about = {
+        'site_name':'earth.world',
+        'author_name':'Yura',
+        'year':'2022'
+    }
+    return about
+
